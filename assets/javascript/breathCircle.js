a8f2c0a0443afe5289ae871b36a82764b53111ed
Ryan Durk <rpdurk@gmail.com>
2021-04-16 14:55:20 -0700

    updated javascript to create circle with object

--- assets/javascript/breathCircle.js
+++ assets/javascript/breathCircle.js
@@ -1,13 +1,41 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-//using radius of canvas, thus it can be adjusted based on screen being used.
-let radius = canvas.height / 2;
-ctx.translate(radius, radius);
-radius = radius * 0.90
-drawClock();
+//let canvas take the entire width/height of window 
+let canvas = document.getElementById("myCanvas");
+const context = canvas.getContext("2d");
 
-function drawClock() {
-    ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
-    ctx.fillStyle = "black";
-    ctx.fill();
+// set window height to match the window on device
+const window_height = window.innerHeight;
+const window_width = window.innerWidth;
+
+// set canvas width/height
+canvas.height = window_height;
+canvas.width = window_width;
+
+// set backgroundColor to something neutral
+canvas.style.backgroundColor = '#f0f8ff';
+
+// create object to use for breath image
+class Circle{
+    constructor(x_point, y_point, radius, color) {
+    this.x_point = x_point;
+    this.y_point = y_point;
+    this.radius = radius;
+    this.color = color;
 }
+    // draw breath circle function 
+    draw(context) {
+        context.beginPath();
+        // canvas function to create circles/arcs.
+        context.arc(this.x_point, this.y_point, this.radius, 0, Math.PI * 2, false);
+        // blue is used for it's association with calm
+        context.strokeStyle ='#0da2ff';
+        context.lineWidth = 15;
+        context.fillStyle = this.color;
+        context.fill();
+        context.stroke();
+        context.closePath();
+    }
+}
+
+// add in preferred values for breath circle
+let circle = new Circle(400, 400, 100, '#0da2ff');
+circle.draw(context);
