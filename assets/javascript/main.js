a033023c50b39cf385bb2340d9949f45ed330b6e
Ryan Durk <rpdurk@gmail.com>
2021-04-16 20:48:37 -0700

    updated javascript for transition time

---
+++ assets/javascript/main.js
@@ -0,0 +1,84 @@
+let endTime = 0;
+
+let inhale = 0;
+let inhaleHold = 0;
+let exhale = 0;
+let exhaleHold = 0;
+let duration = 0;
+
+function start() {
+  // Get values
+  // Time to expand
+  inhale = parseInt(document.getElementById('chosenInhale').innerHTML);
+  // Time to hold at expanded -> do some effect
+  inhaleHold = parseInt(document.getElementById('chosenInhaleHold').innerHTML);
+  // time to shrink
+  exhale = parseInt(document.getElementById('chosenExhale').innerHTML);
+  // Time to hold at shrunk position -> do some effect
+  exhaleHold = parseInt(document.getElementById('chosenExhaleHold').innerHTML);
+  // total time for actions to take place
+  duration = parseInt(document.getElementById('chosenDuration').innerHTML);
+  console.log(inhale, inhaleHold, exhale, exhaleHold, duration);
+
+  // Reset timer
+  endTime = new Date((new Date()).getTime() + (duration * 60 * 1000));
+
+  expand();
+}
+
+function expand() {
+  if (shouldEnd()) {
+    shrink(true);
+  }
+
+  // do animation
+  console.log("Expanding!");
+
+  // Make the circle expand
+  setTimeout(function() {
+    holdExpand();
+  }, inhale * 1000);
+}
+
+function holdExpand() {
+  if (shouldEnd()) {
+    shrink(true);
+  }
+
+  // do animation
+  console.log("Holding after expan!");
+  
+  setTimeout(function() {
+    shrink();
+  }, inhaleHold * 1000);
+}
+
+function shrink(end) {
+  // Do animation
+  console.log("Shrinking!");
+  
+  if (end || shouldEnd()) {
+    return;
+  }
+
+  setTimeout(function() {
+    holdShrink();
+  }, exhale * 1000);
+}
+
+function holdShrink() {
+  // do animation
+  console.log("Holding after shrink!");
+
+  if (shouldEnd()) {
+    shrink(true);
+  }
+
+  setTimeout(function() {
+    expand();
+  }, exhaleHold * 1000);
+}
+
+function shouldEnd() {
+  return (new Date()) > endTime;
+}
