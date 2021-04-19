9f36594eb141bd826f0d3b839c4868f0e1cb55a1
Ryan Durk <rpdurk@gmail.com>
2021-04-18 19:49:20 -0700

    seamless background transition updated

--- assets/javascript/navScript.js
+++ assets/javascript/navScript.js
@@ -1,22 +1,11 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-
-    const container = document.getElementsByClassName('container')[0];
-    container.style.filter = 'brightness(85%)';
-    // const mainContainer = document.getElementById('main');
-    // mainContainer.style.filter = 'brightness(85%)';
-    document.body.style.filter = 'brightness(85%)';
-    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-    // document.body.style.backgroundColor = "white";
-    const container = document.getElementsByClassName('container')[0];
-    container.style.filter = '';
-    // const mainContainer = document.getElementById('main')[0];
-    // mainContainer.style.filter = '';
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "#f0f8ff";
   }
